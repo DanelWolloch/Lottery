afd8302ecf88f757010df738937ecec73430f66c
or ben shoef <or.benshoef@gmail.com>
2012-10-03 19:20:26 +0200

    Add error handling and no winning

--- Lottery.js
+++ Lottery.js
@@ -107,25 +107,62 @@ function resetMessages() {
     });
 }
 
-function Check() {
+function Check()
+{
+    // Clear the messages in the GUI
     resetMessages();
 
+    // Get the URL query to the pais site using yql
     var select = "select * from html where url="
     var theUrl = GetUrl(getTablesString(), getSubGame());
-
     var yqlQuery = "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(select + '"' + theUrl + '"') + "&callback=?";
 
-    $.getJSON(yqlQuery, function (data) {
-        var winnigPrice = data.results[0].split("<td class=\"PaisSeventh\">")[1].split('<p>')[1].split('</p>')[0];
-        
-        var isChecked = $('#doubleLotoCheckbox:checked').val() ? true : false;
-        if (isChecked)
+    // Get the HTML page with the response for the loto results
+    // Url: The url to request
+    // DataType: Jason request type.
+    // Success: The function that parsing the html response and show the winning price
+    // Error: Error handling when the website is unreachable
+    // Timeout: The timeout to get the response
+    $.ajax(
+    {
+        url: yqlQuery,
+        dataType: 'json',
+        success: function (data)
         {
-            winnigPrice *= 2;
-        }
+            try
+            {
+                // Parse the winning price
+                var winnigPrice = data.results[0].split("<td class=\"PaisSeventh\">")[1].split('<p>')[1].split('</p>')[0];
+            }
+            catch (ex)
+            {
+                // If no winning set to 0
+                winnigPrice = 0;
+            }
 
-        $('#winningPrice').text("זכית ב- " + winnigPrice + " שקלים!");
-        $('#winningPrice').fadeIn('slow');
+            // Check if double loto checked
+            var isChecked = $('#doubleLotoCheckbox:checked').val() ? true : false;
+            if (isChecked)
+            {
+                winnigPrice *= 2;
+            }
+
+            // Show the winning price
+            $('#winningPrice').text("זכית ב- " + winnigPrice + " שקלים!");
+            $('#winningPrice').fadeIn('slow');
+        },
+        error: function (data)
+        {
+            try
+            {
+                throw ({ message: "אירעה שגיאה בזמן הפנייה לשרת. אנא בדוק חיבור אינטרנט." });
+            }
+            catch (ex)
+            {
+                handleError(ex);
+            }
+        },
+        timeout: 7000
     });
 }
 
